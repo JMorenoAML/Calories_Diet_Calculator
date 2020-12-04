e71d427803383de3540ad90bdf2483a340afb130
Jose Luis Moreno <jmorenoaml@gmail.com>
2020-12-04 11:39:14 +0100

    first idea of user data retrieving system imported module User.py first commit.

--- venv/Scripts/Calculator module.py
+++ venv/Scripts/Calculator module.py
@@ -11,11 +11,37 @@ EXERCISE DONE (We can aproximate Cals depending on kind and duration of sport, a
 OR, CALORIES BURNT (or we can directly input the burnt calories every day, integer)
 '''
 
+from model import User
+
 def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de google con translate.
     print("Welcome to Diet&Calories calculator")
-    weigth= input("Write your weigth in Kg:")
-    input()
+    User.alias = input('What is your name?')
+    print("Nice to meet you {}".format(User.alias))
+    print("I need some data from before we prepare your ideal Diet")
 
+    while True:
+        try:
+            User.age = input("How old are you? (in years, numerical)")
+        if (not (type(User.age) == int) or (User.age < 0)):
+            print("Please write it again, this time as positive integer numerical value")
+            print(" For example:  24  ")
+            continue
+        else:
+            break
 
+    while True:
+        try:
+            User.gender = input("How old are you? (in years, numerical)")
+        if (not (type(User.age) == int) or (User.age < 0)):
+            print("Please write it again, this time as positive integer numerical value")
+            print(" For example:  24  ")
+            continue
+        else:
+            break
 
+    while Tr
+
+    gender = input('Is your gender is male, write 1. For female write 2: ')
+    weight = int(input('What is your weight: '))
+    height = int(input('What is your height in inches: '))
 
