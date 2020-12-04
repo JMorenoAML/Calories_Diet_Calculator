577cf2360e8232e8f1f745fe84461d47ba3b241a
Jose Luis Moreno <jmorenoaml@gmail.com>
2020-12-04 12:15:38 +0100

    forgot initializing my object with the Class User :)

--- venv/Scripts/Calculator module.py
+++ venv/Scripts/Calculator module.py
@@ -13,6 +13,8 @@ OR, CALORIES BURNT (or we can directly input the burnt calories every day, integ
 
 from model import User
 
+new_user = User()
+
 def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de google con translate.
     print("Welcome to Diet&Calories calculator")
     User.alias = input('What is your name?')
@@ -21,8 +23,8 @@ def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de
     ####### AGE 1/5 #######
     while True:
         try:
-            User.age = input("How old are you? (in years, numerical)")
-        if (not (type(User.age) == int) or (User.age < 0)):
+            new_user.age = input("How old are you? (in years, numerical)")
+        if (not (type(new_user.age) == int) or (new_user.age <= 0)):
             print("Please write it again, this time as positive integer numerical value")
             print(" For example:  24  ")
             continue
@@ -31,22 +33,22 @@ def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de
     ####### GENDER 2/5 #######
     while True:
         try:
-            gen_dummy = input("Are you female or male? 1 = male, 0 = female")
-        if ((gen_dummy != 1) or (gen_dummy != 0)):
+            gender_dummy = input("Are you female or male? 1 = male, 0 = female")
+        if ((gender_dummy != 1) or (gender_dummy != 0)):
             print("Please write 0 for female, or 1 for male")
             print("Try again!")
             continue
-        elif (gen_dummy == 1):
-            User.gender == "male"
+        elif (gender_dummy == 1):
+            new_user.gender == "male"
             break
         else:
-            gen_dummy == 0:
-            User.gender == "female"
+
+            new_user.gender == "female"
             break
 
     while True:
         try:
-            User.size
+            new_user.size
 
     gender = input('Is your gender is male, write 1. For female write 2: ')
     weight = int(input('What is your weight: '))
