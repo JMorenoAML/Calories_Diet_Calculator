1f9f730b20b476fc0d43940ce40112ec62e6624b
Jose Luis Moreno <jmorenoaml@gmail.com>
2020-12-04 11:49:26 +0100

    wrote until age, and gender. 2/5

--- venv/Scripts/Calculator module.py
+++ venv/Scripts/Calculator module.py
@@ -18,7 +18,7 @@ def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de
     User.alias = input('What is your name?')
     print("Nice to meet you {}".format(User.alias))
     print("I need some data from before we prepare your ideal Diet")
-
+    ####### AGE 1/5 #######
     while True:
         try:
             User.age = input("How old are you? (in years, numerical)")
@@ -28,18 +28,25 @@ def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de
             continue
         else:
             break
+    ####### GENDER 2/5 #######
+    while True:
+        try:
+            gen_dummy = input("Are you female or male? 1 = male, 0 = female")
+        if ((gen_dummy != 1) or (gen_dummy != 0)):
+            print("Please write 0 for female, or 1 for male")
+            print("Try again!")
+            continue
+        elif (gen_dummy == 1):
+            User.gender == "male"
+            break
+        else:
+            gen_dummy == 0:
+            User.gender == "female"
+            break
 
     while True:
         try:
-            User.gender = input("How old are you? (in years, numerical)")
-        if (not (type(User.age) == int) or (User.age < 0)):
-            print("Please write it again, this time as positive integer numerical value")
-            print(" For example:  24  ")
-            continue
-        else:
-            break
-
-    while Tr
+            User.size
 
     gender = input('Is your gender is male, write 1. For female write 2: ')
     weight = int(input('What is your weight: '))
