303a5de13749be3ac6efd149462bc4329bac9c55
Jose Luis Moreno <jmorenoaml@gmail.com>
2020-12-10 11:04:22 +0100

    doubts on use of User_ID rethink how are we going to forbid user to create duplicated accounts/User_Ids

--- venv/Scripts/Calculator module.py
+++ venv/Scripts/Calculator module.py
@@ -19,11 +19,11 @@ new_user = User()
 def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de google con translate.
     print("Welcome to Diet&Calories calculator")
     User.alias = input('What is your name?')
-    print("Nice to meet you {}".format(User.alias))
+    print("Nice to meet you {}".format(new_user.alias))
     print("I need some data from before we prepare your ideal Diet")
     ####### AGE 1/5 #######
     while True:
-        new_user.age = input("How old are you? (in years, numerical)")
+        new_user.age = int(input("How old are you? (in years, numerical)"))
 
         if type(new_user.age) != int or new_user.age >= 0:
             print("Please write it again, this time as positive integer numerical value")
@@ -33,7 +33,7 @@ def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de
             break
     ####### GENDER 2/5 #######
     while True:
-        gender_dummy = input("Are you female or male? 1 = male, 0 = female")
+        gender_dummy = int(input("Are you female or male? 1 = male, 0 = female"))
         if ((gender_dummy != 1) or (gender_dummy != 0)):
             print("Please write 0 for female, or 1 for male")
             print("Try again!")
@@ -47,7 +47,42 @@ def user_info_retriever():    #nota: hacerlo multilanguage usando spreadsheet de
             break
     return new_user
 
-print("tus datos de entrada son:")
+    ####### SIZE 3/5 #######
+    while True:
+        new_user.size = int(input("Write your size in cms (integer value)"))
+        if ((new_user.size <=0) or (new_user.size > 260)):
+            print("I don't think that is your correct heigth in cms")
+            print("Try again!")
+
+            continue
+        elif (type(new_user.size != int ))
+            continue
+
+        else:
+            break
+
+    ####### WEIGTH 4/5 #######
+    while True:
+        new_user.weight = input("Write your weigth in Kgs ")      # Disclaimer, cómo saber que lo que meten es realmente
+        if ((new_user.size <= 0) or (new_user.size > 260)):       # Kgs y no Lbs
+            print("I don't think that is your correct weigth in Kgs")
+            print("Try again!")
+
+            continue
+        elif (type(new_user.size != int) or type(new_user.size != float))
+            continue
+
+        else:
+            break
+
+    ###### User_ID ######
+
+    # Still got no clear if I want to have this inside this function, or be called externally
+    # the idea is this ID is unique, and must be incremented every time the function is called.
+    # maybe nested functions?? Also I need a way to not allow someone already registered and with an assigned ID,
+    # registering again (this would create duplicated entries )
+
+print("your data is:")
 print(new_user.age," years old\nyour gender is ", new_user.gender)
 
 #####TEST ZONE
