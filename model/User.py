5190e34bd92e96a5d664172d4695d9d99dd0b0f0
Jose Luis Moreno <jmorenoaml@gmail.com>
2020-12-12 09:26:45 +0100

    minor fix

--- model/User.py
+++ model/User.py
@@ -1,6 +1,6 @@
 class User:
     
-    def __init__(self, age, weight, size, gender, alias, user_id):
+    def __init__(self, age, weight, size, gender, alias):
         self.age = age
         self.weight = weight
         self.size = size
